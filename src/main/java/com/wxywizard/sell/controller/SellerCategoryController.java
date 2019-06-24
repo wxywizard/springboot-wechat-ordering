079b7464fe188399b181ceada48e51fb8417f9b7
wxywizard <wxy12125@vip.qq.com>
2019-06-24 15:09:18 +0800

    商品类目相关接口和相关测试

--- src/main/java/com/wxywizard/sell/controller/SellerCategoryController.java
+++ src/main/java/com/wxywizard/sell/controller/SellerCategoryController.java
@@ -7,7 +7,6 @@ import com.wxywizard.sell.service.CategoryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
